8fc8767406bf707cc65c4aad22551cec4351b946
bassam edelbi <bassamedelbi360@gmail.com>
2021-01-02 09:58:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const newTaskUl = document.getElementById("tasks");
   newTaskForm.addEventListener("submit", createNewTask);
 });
+const createNewTask = event => {
+  event.preventDefault();
+  //stop form from trying to submit
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById("tasks").appendChild(task);
+};
